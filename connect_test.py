24a84251b45beb0ceb34a184c6d203611c1858de
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-08 18:05:02 +0200

    Connect test

---
+++ connect_test.py
@@ -0,0 +1,8 @@
+from mysql.connector import connect, Error
+
+try:
+    with connect(host="localhost", user='test', password='test') as conn:
+        print(conn)
+
+except Error as e:
+    print(e)
