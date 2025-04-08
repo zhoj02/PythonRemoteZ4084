dfc36578f24af6da6115f0bbdd4229218fc854a8
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-08 19:49:08 +0200

    Vysvetlim with

--- 2025_04_08/vysvetlim_with.py
+++ 2025_04_08/vysvetlim_with.py
@@ -2,4 +2,10 @@
 from mysql.connector import connect
 
 pripojeni = connect(host="localhost", user='root', password='YourNewPassword')
+pripojeni.close()
 print(pripojeni.user)
+# Context Manager
+with connect(host="localhost", user='root', password='YourNewPassword') as pripojeni:
+    print(pripojeni.user)
+
+print("Jsem jinde")
