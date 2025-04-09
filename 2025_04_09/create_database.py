7d38007dd26be4a9e8263d94410665e009ab387f
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-09 19:16:26 +0200

    Create database

--- 2025_04_09/create_database.py
+++ 2025_04_09/create_database.py
@@ -3,6 +3,6 @@ from mysql.connector import connect
 with open("heslo.txt", "r") as file:
     password = file.read()
 
-with connect(user="root", password=password, database="mysql") as conn:
+with connect(user="root", password=password) as conn:
     cursor = conn.cursor()
     cursor.execute("CREATE DATABASE online_movie_rating")
