888fe33b32ca1ab0c22fef1968452d2ab7558bdb
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-09 18:32:05 +0200

    Connect test

--- 2025_04_09/connect_to_database.py
+++ 2025_04_09/connect_to_database.py
@@ -5,3 +5,5 @@ with open("heslo.txt", "r") as file:
 
 with connect(user="root", password=password) as conn:
     cursor = conn.cursor()
+    cursor.execute("SHOW DATABASES")
+    print(cursor.fetchall())
