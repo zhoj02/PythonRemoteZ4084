0eefd8593576293a771e426d4f193defcb92efed
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-09 20:13:15 +0200

    Create Table - 2

---
+++ 2025_04_09/sql_injection.py
@@ -0,0 +1,13 @@
+from mysql.connector import connect
+
+with open("heslo.txt", "r") as file:
+    password = file.read()
+
+jmeno_filmu = "Forrest Gump-'; drop table movies"
+
+with connect(user="root", password=password, database="online_movie_rating") as conn:
+    cursor = conn.cursor()
+    cursor.execute(f"""
+        SELECT * FROM movies WHERE title = '{jmeno_filmu}'
+        """)
+    print(cursor.fetchall())
