81cd71d13f8e666d50ad38718575fd905ce4e51a
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-09 17:52:58 +0200

    Connect test

--- 2025_04_08/connect_test.py
+++ 2025_04_08/connect_test.py
@@ -1,8 +1,13 @@
 #pip install mysql-connector-python
+
+
 from mysql.connector import connect, Error
 
+with open("heslo.txt","r") as file:
+    password = file.read()
+
 try:
-    with connect(host="localhost", user='root', password='YourNewPassword') as conn:
+    with connect(host="localhost", user='root', password=password) as conn:
         print(conn)
 
 except Error as e:
