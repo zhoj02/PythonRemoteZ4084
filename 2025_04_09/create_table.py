bf9738fff059176c638c79ede49849c42b7e1903
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-09 19:19:29 +0200

    Create Table - 1

--- 2025_04_09/create_table.py
+++ 2025_04_09/create_table.py
@@ -3,6 +3,11 @@ from mysql.connector import connect
 with open("heslo.txt", "r") as file:
     password = file.read()
 
-with connect(user="root", password=password) as conn:
+with connect(user="root", password=password, database="online_movie_rating") as conn:
     cursor = conn.cursor()
-    cursor.execute("CREATE DATABASE online_movie_rating")
+    cursor.execute("""
+        CREATE TABLE movies(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(100)
+        )
+        """)
