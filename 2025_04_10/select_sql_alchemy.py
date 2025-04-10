198cd9dbac09603a51c36d1bc76c381e693f705b
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-10 20:31:26 +0200

    Password

---
+++ 2025_04_10/select_sql_alchemy.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine, text, String, Integer, Date, Column
+from database_definition import Actor
+from sqlalchemy.orm import sessionmaker
+
+with open("heslo.txt", "r") as file:
+    password = file.read()
+
+# connection string
+db = create_engine(f'mysql+mysqlconnector://root:{password}@localhost:3306/online_movie_rating')
+
+Session = sessionmaker(bind=db)
+
+session = Session()
+
+
+print(session.execute(Actor))
