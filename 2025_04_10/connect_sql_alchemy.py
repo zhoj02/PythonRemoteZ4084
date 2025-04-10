3094c20fcaec64c0a7548e3563d7a1361d041545
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-10 19:09:32 +0200

    Renamed paramater map results

---
+++ 2025_04_10/connect_sql_alchemy.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# connection string
+db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost:3306/')
+
+Base = declarative_base()
+Session = sessionmaker(bind=db)
+
+session = Session()
+#test connection with SELECT 1
+result = session.execute(text("SELECT 1"))
+for row in result:
+    print(row[0])
--- 2025_04_10/sql_injection.py
+++ 2025_04_10/sql_injection.py
@@ -7,6 +7,7 @@ jmeno_filmu = "Forrest Gump'; DROP TABLE movies; -- "
 
 with connect(user="root", password=password, database="online_movie_rating") as conn:
     cursor = conn.cursor()
+
     cursor.execute(f"""
         SELECT * FROM movies WHERE title = '{jmeno_filmu}'
         """)
