c23977d9c35e45074a574f28b83447efa363b993
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-10 20:00:29 +0200

    Password

--- 2025_04_10/create_table_sql_alchemy.py
+++ 2025_04_10/create_table_sql_alchemy.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine, text, String, Integer, Date, Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+with open("heslo.txt", "r") as file:
+    password = file.read()
+
 # connection string
-db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost:3306/online_movie_rating')
+db = create_engine(f'mysql+mysqlconnector://root:{password}@localhost:3306/online_movie_rating')
 
 Base = declarative_base()
 Session = sessionmaker(bind=db)
