5e6534174d30af1cc451488416c0303e6a633d0e
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-22 18:46:18 +0200

    Repeat sql alchemy connect

--- 2025_04_22/connect_sql_alchemy.py
+++ 2025_04_22/connect_sql_alchemy.py
@@ -1,7 +1,14 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
 
 # connection string
 db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost:3306/')
 
+Session = sessionmaker(bind=db)
+
+session = Session()
+
+result = session.execute(text("SELECT 1"))
+for row in result:
+    print(row[0])
