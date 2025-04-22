f562bfabfa5283eb5dc3d864c1fd661cc25839ff
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-22 20:16:51 +0200

    SQL alchemy - table names

---
+++ 2025_04_22/insert_shop.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+from shop_definition import Base, CiselnikZbozi
+db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost:3306/sql_orm')
+
+#insert mock data
+sessionmaker = sessionmaker(bind=db)
+session = sessionmaker()
+# Create mock data
+session.add(CiselnikZbozi(id=1, nazev_zbozi="Test Product", carovy_kod=1234567890123))
+
+
+
+
+
