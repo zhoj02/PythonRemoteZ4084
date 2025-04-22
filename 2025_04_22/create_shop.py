452a4d9706a990b98a8221d2f1e4094b635dbca1
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-22 19:51:56 +0200

    SQL alchemy - objects

--- 2025_04_22/create_shop.py
+++ 2025_04_22/create_shop.py
@@ -3,21 +3,21 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class CiselnikZbozi:
+class CiselnikZbozi(Base):
     # Primary Key
     id = Column(Integer, primary_key=True)
     nazev_zbozi = Column(String)
     carovy_kod = Column(Integer)
 
 
-class Zasoby:
+class Zasoby(Base):
     id = Column(Integer, primary_key=True)
     #Foreign Key
     cislo_zbozi = Column(Integer, ForeignKey('CiselnikZbozi.id'))
     pocet = Column(Integer)
 
 
-class Prodeje:
+class Prodeje(Base):
     id = Column(Integer, primary_key=True)
     cislo_zbozi = Column(Integer, ForeignKey('CiselnikZbozi.id'))
     cas_prodeje = Column(DateTime)
