77cbc446bb766b0c11648ec176c34b41d5c3fbbd
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-22 20:33:32 +0200

    Insert hodne dat

--- 2025_04_22/select_shop.py
+++ 2025_04_22/select_shop.py
@@ -8,15 +8,7 @@ db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost:3306/s
 #insert mock data
 sessionmaker = sessionmaker(bind=db)
 session = sessionmaker()
-# Create mock data
-session.add(CiselnikZbozi(id=1, nazev_zbozi="Test Product", carovy_kod=15758))
-# Commit the transaction
-session.commit()
-
-
-# (id, nazev_zbozi, carovy_kod) values (1, 'Test Product', 1234567890123)
-
-
-
-
-
+# Select
+result = session.query(CiselnikZbozi).all()
+for row in result:
+    print(row.id, row.nazev_zbozi, row.carovy_kod)
