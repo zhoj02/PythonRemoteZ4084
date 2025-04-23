00fd71cb1727545833f7feb3cd07fa0969849cc8
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-23 19:00:48 +0200

    Insert hodne dat

--- 2025_04_22 + 23/insert_hodne_dat.py
+++ 2025_04_22 + 23/insert_hodne_dat.py
@@ -4,18 +4,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from shop_definition import CiselnikZbozi
+from shop_definition import CiselnikZbozi, Zasoby
 
 db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost:3306/sql_orm')
 Session = sessionmaker(bind=db)
 session = Session()
 
+# session.add_all(
+#     [
+#         CiselnikZbozi(id=3, nazev_zbozi="Banany ruzove", carovy_kod=19582),
+#         CiselnikZbozi(id=4, nazev_zbozi="Maslo", carovy_kod=18975),
+#         CiselnikZbozi(id=5, nazev_zbozi="Redkvicky", carovy_kod=69750),
+#         CiselnikZbozi(id=6, nazev_zbozi="Testoviny", carovy_kod=47800),
+#
+#     ]
+#
+# )
+
 session.add_all(
     [
-        CiselnikZbozi(id=3, nazev_zbozi="Banany ruzove", carovy_kod=19582),
-        CiselnikZbozi(id=4, nazev_zbozi="Maslo", carovy_kod=18975),
-        CiselnikZbozi(id=5, nazev_zbozi="Redkvicky", carovy_kod=69750),
-        CiselnikZbozi(id=6, nazev_zbozi="Testoviny", carovy_kod=47800),
+        Zasoby(cislo_zbozi=3, pocet=800),
 
     ]
 
