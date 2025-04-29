8cb7750834869e26eb820bed93130213f61cc6b1
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-29 18:02:13 +0200

    Ukol 1 - Automobilka databaze

---
+++ 2025_04_29/ukol4_insert.py
@@ -0,0 +1,5 @@
+# Ukol 4 - Vlozit do tabulky data
+# Do tabulky model auta vlozte 10 modelu aut. Ale pomoci for loopu.
+# Do tabulky dily vlozte 100 dilu. Ale pomoci for loopu.
+# Do tabulky vyrobni linka vlozte 10 vyrobni linek. Ale pomoci for loopu.
+# Do tabulky vyrobni proces vlozte 2 vyrobni procesu.
