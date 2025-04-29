e88efa4553c6e8de1cf811c05e66c52e66bcd1ae
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-29 17:49:59 +0200

    Ukol 1 - Automobilka databaze

--- 2025_04_29/ukol2-definici databaze.py
+++ 2025_04_29/ukol2-definici databaze.py
@@ -2,4 +2,4 @@
 # V tomto ukolu, nesmite se do databaze pripojit.
 # Musite jen vytvorit pomoci declarative base 4 classy, ktere budou predstavovat budouci tabulky.
 # Jedna class bude bude model auta, druha dily, treti vyrobni linka a ctvrta vyrobni proces.
-# kazda class musi mit minimalne 4 atributy, primarni klic a minimalne jeden cizi lic.
+# kazda class musi mit minimalne 4 atributy, primarni klic a minimalne jeden cizi klic.
