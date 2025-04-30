bc7cbf7b5d61b26052500b8ba31f5ca15f2807c1
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-30 17:51:54 +0200

    Relationship - create database

---
+++ 2025_04_30/create_database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+
+# connection string
+db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost:3306/')
+
+Session = sessionmaker(bind=db)
+
+session = Session()
+
+session.execute(text("CREATE DATABASE relationships"))
