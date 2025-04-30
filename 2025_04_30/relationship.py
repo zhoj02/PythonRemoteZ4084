54a870c3af93af35c7d4ae9b5631ad76b6c01e96
jan.zhouf <jan.zhouf@resulmatic.com>
2025-04-30 18:00:51 +0200

    Relationship - create database

--- 2025_04_30/relationship.py
+++ 2025_04_30/relationship.py
@@ -3,10 +3,30 @@ from sqlalchemy.orm import sessionmaker
 
 
 # connection string
-db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost:3306/')
+db = create_engine('mysql+mysqlconnector://root:YourNewPassword@localhost:3306/relationships')
 
-Session = sessionmaker(bind=db)
+# create salary and employye class with Base, import declarative_base
+# use relantionship in the class from sql alchemy
 
-session = Session()
+from sqlalchemy.ext.declarative import declarative_base
 
-session.execute(text("CREATE DATABASE relationships"))
+Base = declarative_base()
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Employee(Base):
+    __tablename__ = 'employee'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    salary_id = Column(Integer, ForeignKey('salary.id'))
+
+    salary = relationship("Salary", back_populates="employees")
+
+
+class Salary(Base):
+    __tablename__ = 'salary'
+    id = Column(Integer, primary_key=True)
+    amount = Column(Integer)
+
+    employees = relationship("Employee", back_populates="salary")
